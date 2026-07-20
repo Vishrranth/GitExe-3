525bfc078d2714c1146f7c57286fcd5d67a9e0f3
JaswanthSri <11jaswanth@gmail.com>
2026-07-20 06:47:20 +0530

    Added login module

--- Src/login.py
+++ Src/login.py
@@ -1,2 +1,9 @@
 def login(username, password):
-    print("Login successful")
+    if username == "admin" and password == "1234":
+        print("Login successful")
+    else:
+        print("Invalid username or password")
+
+
+if __name__ == "__main__":
+    login("admin", "1234")
